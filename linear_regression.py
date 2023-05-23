e7a6b1d84099580e5a6ac95b33d2e86fe64187c4
rohithpeddi7 <peddi.rohith.cse21@itbhu.ac.in>
2023-05-23 15:07:58 +0530

    Added Logistic Regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 class LinearRegression:
     def __init__(self):
@@ -18,7 +19,7 @@ class LinearRegression:
         self.trainY = trainY
         self.m = trainX.shape[0]
         self.history = []
-        for _ in range(epochs):
+        for _ in tqdm(range(epochs)):
             self.history.append(self._cost_function())
             self._gradient_descent()
         return self.history
@@ -37,10 +38,29 @@ class LinearRegression:
         c = np.sum(np.multiply((np.dot(self.w,self.trainX.T)+self.b-self.trainY).reshape((-1,1)), self.trainX),axis=0)/(self.m)
         self.w = self.w - self.learning_rate*c
         self.b = self.b - self.learning_rate*np.sum((np.dot(self.w,self.trainX.T)+self.b-self.trainY))/self.m
+
+    def predict(self, test_X):
+        test_x = np.array(test_X)
+        test_x = test_x.reshape((-1,self.w.shape[0]))
+        return np.dot(self.w,test_x.T)+self.b
         
 if __name__ == '__main__':
-    ok = LinearRegression()
-    _train_x = [[1,2], [3,10], [7,6], [20,3], [9,8], [-2,-4], [1,100]]
-    _train_y = [7, -3, 17, 62, 19, 10, -189]
-    ok.fit(_train_x,_train_y)
-    print(ok.w,ok.b)
+    # ok = LinearRegression()
+    # _train_x = [[1,2], [3,10], [7,6], [20,3], [9,8], [-2,-4], [1,100]]
+    # _train_y = [7, -3, 17, 62, 19, 10, -189]
+    # ok.fit(_train_x,_train_y)
+    # print(ok.w,ok.b)
+    # print(ok.predict([10,2]))
+    from sklearn.datasets import fetch_california_housing as load_boston
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import mean_squared_error
+    from sklearn.preprocessing import StandardScaler
+
+    data = load_boston()
+    scaler = StandardScaler()
+    data.data = scaler.fit_transform(data.data)
+    trainX, testX, trainY, testY = train_test_split(data.data, data.target, test_size=0.2)
+    meow = LinearRegression()
+    meow.fit(trainX, trainY, learning_rate=0.001, epochs=6000)
+    print(mean_squared_error(testY, meow.predict(testX)))
+
--- logistic_regression.py
+++ logistic_regression.py
@@ -4,7 +4,7 @@ class LogisticRegression():
     def __init__(self):
         pass
 
-    def fit(self, trainX, trainY, learning_rate = 0.001, epochs = 10000):
+    def fit(self, trainX, trainY, learning_rate = 0.0001, epochs = 10000):
         """
         Function to train the logistic model. Just pass in trainX, trainY and it will do the rest.
         """
@@ -21,4 +21,41 @@ class LogisticRegression():
             self.history.append(self._cost_function())
             self._gradient_descent()
         return self.history
+    
+    def _cost_function(self):
+        """
+        Categorical cross entropy cost function
+        """
+        pred_y = self.predict(self.trainX)
+        return np.sum((-self.trainY*np.log(pred_y+1e-9) -(1-self.trainY)*np.log(1+1e-9-pred_y)))/(self.m)
+    
+    def predict(self, test_X):
+        """
+        Predicts the output for the given input
+        """
+        test_x = np.array(test_X)
+        test_x = test_x.reshape((-1,self.w.shape[0]))
+        return 1/(1+np.exp(-(np.dot(self.w,test_x.T)+self.b)))
+
+    def _gradient_descent(self):
+        """
+        Does gradient descent with the given learning rate
+        """
+        pred_y = self.predict(self.trainX)
+        self.w = self.w - self.learning_rate*((pred_y-self.trainY).reshape((-1,1))*self.trainX).sum(axis=0)/self.m
+        self.b = self.b - self.learning_rate*np.sum(pred_y - self.trainY)/self.m
+
+        
+if __name__ == '__main__':
+    from sklearn.datasets import load_breast_cancer as lbc
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
+
+    data = lbc()
+    trainX, testX, trainY, testY = train_test_split(data.data, data.target, test_size=0.2)
+    meow = LogisticRegression()
+    meow.fit(trainX, trainY, learning_rate=0.0001, epochs=10000)
+    print(accuracy_score(testY, meow.predict(testX)>0.5))
+    
+    
     
